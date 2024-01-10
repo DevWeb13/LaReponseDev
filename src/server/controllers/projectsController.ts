1af65eb4eaa3ac7a78c02149b62a4f0912d46f6d
DevWeb13 <giuliano.loic@sfr.fr>
2024-01-10 14:59:24 +0100

    Refactor projectsController and Project model imports

--- src/server/controllers/projectsController.ts
+++ src/server/controllers/projectsController.ts
@@ -1,18 +1,21 @@
 // src/server/controllers/projectsController.ts
 
-import Project from '../models/Project';
-import connect from '../connect-db';
+const Project = require('../models/Project');
+const connect = require('../connect-db');
+
 import { ProjectType } from '~/types/project';
 
 export const getAllProjects = async (): Promise<ProjectType[] | null> => {
   await connect();
   const projects = await Project.find();
   if (projects) {
-    const transformedProjects: ProjectType[] = projects.map((project) => ({
-      ...project.toObject(),
-      _id: project._id.toString(),
-      pages: [],
-    }));
+    const transformedProjects: ProjectType[] = projects.map(
+      (project: { toObject: () => any; _id: { toString: () => any } }) => ({
+        ...project.toObject(),
+        _id: project._id.toString(),
+        pages: [],
+      })
+    );
 
     return transformedProjects;
   }
@@ -28,10 +31,12 @@ export const getOneProject = async (
     const transformedProject: ProjectType = {
       ...project.toObject(),
       _id: project._id.toString(),
-      pages: project.pages.map((page) => ({
-        ...page.toObject(),
-        _id: page._id?.toString(),
-      })),
+      pages: project.pages.map(
+        (page: { toObject: () => any; _id: { toString: () => any } }) => ({
+          ...page.toObject(),
+          _id: page._id?.toString(),
+        })
+      ),
     };
     return transformedProject;
   }
