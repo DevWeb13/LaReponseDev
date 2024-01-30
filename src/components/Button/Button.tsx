032ce9ca26bce913982d625cb0e9cec7bd4afe04
DevWeb13 <giuliano.loic@sfr.fr>
2024-01-31 00:45:09 +0100

    test

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,22 +1,22 @@
 // src/components/Button/Button.tsx
 
-import { Slot, component$ } from '@builder.io/qwik';
+import { Slot, component$, $ } from '@builder.io/qwik';
 import styles from './button.module.css';
 
 interface ButtonProps {
   type?: 'button' | 'submit' | 'reset'; // Type rendu optionnel
   class?: string;
-  onClick$?: () => void | Promise<void> | Promise<unknown>;
+  onClick: () => void | Promise<void> | Promise<unknown>;
 }
 
 export default component$<ButtonProps>(
-  ({ type = 'button', class: cls, onClick$, ...rest }) => {
+  ({ type = 'button', class: cls, onClick, ...rest }) => {
     return (
       <button
         {...rest}
         type={type} // Utilisation de la prop type avec la valeur par défaut
         class={`${styles.button} ${cls}`}
-        onClick$={onClick$}>
+        onClick$={$(onClick)}>
         <Slot />
       </button>
     );
